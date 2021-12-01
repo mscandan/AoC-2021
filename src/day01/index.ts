b22a9313f784cb2a7604b9a7e2af275ee94a76e4
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2021-12-01 15:21:18 +0300

    add solution for day1/part1

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,3 +1,15 @@
+import { data } from "./data";
+
 export const solution = () => {
-  console.log("hello world");
+  console.log(part1(data));
+};
+
+const part1 = (data: Array<number>): number => {
+  let incCount = 0;
+  for (let i = 1; i < data.length; i++) {
+    if (data[i] > data[i - 1]) {
+      incCount++;
+    }
+  }
+  return incCount;
 };
