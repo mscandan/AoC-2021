2c660d2b09f0037b703dc8165e97b4073ca8fbc1
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2021-12-01 19:16:41 +0300

    add solution for day1/part2

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,13 +1,28 @@
 import { data } from "./data";
 
 export const solution = () => {
-  console.log(part1(data));
+  console.log("Day 1 Part 2: ", part1(data));
+  console.log("Day 1 Part 2: ", part2(data));
 };
 
 const part1 = (data: Array<number>): number => {
   let incCount = 0;
   for (let i = 1; i < data.length; i++) {
     if (data[i] > data[i - 1]) {
+      incCount++;
+    }
+  }
+  return incCount;
+};
+
+const part2 = (data: Array<number>): number => {
+  let incCount = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (
+      data[i] + data[i + 1] + data[i + 2] <
+        data[i + 1] + data[i + 2] + data[i + 3] &&
+      i + 3 <= data.length
+    ) {
       incCount++;
     }
   }
