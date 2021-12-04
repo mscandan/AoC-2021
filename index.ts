a10021d70248e3dc9fa865f497908776fd0b292c
M. Selcuk Candan <mehmetselcukcandan@icloud.com>
2021-12-04 19:55:09 +0300

    add solution for day4

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { solution as dayOneSolution } from "./src/day01";
 import { solution as dayTwoSolution } from "./src/day02";
 import { solution as dayThreeSolution } from "./src/day03";
+import { solution as dayFourSolution } from "./src/day04";
 
 dayOneSolution();
 dayTwoSolution();
 dayThreeSolution();
+dayFourSolution();
---
+++ src/day04/index.ts
@@ -0,0 +1,54 @@
+import { readFileSync } from "fs";
+
+export const sum = (xs: number[]): number => xs.reduce((sum, x) => sum + x, 0);
+
+export const range = (start: number, end: number, step = 1): number[] =>
+  [...Array(end - start).keys()]
+    .map((i) => start + i * step)
+    .filter((x) => x < end);
+
+type Row = (number | null)[];
+type Board = Row[];
+
+const filterBoard = (board: Board, draw: number): Board =>
+  board.map((row) => row.map((n) => (n !== draw ? n : null)));
+
+const cardComplete = (card: Board): boolean => {
+  const complete = (xs: Row) => xs.every((x) => x === null);
+  const rowComplete = card.some((r) => complete(r));
+  const colComplete = range(0, card.length)
+    .map((c) => card.map((l) => l[c]))
+    .some((c) => complete(c));
+  return rowComplete || colComplete;
+};
+
+const scoreBoard = (board: Board, draw: number): number =>
+  draw * sum(board.flat().filter((n) => n !== null) as number[]);
+
+const input = readFileSync("src/day04/data").toString().split("\n\n");
+const drawn = input[0].split(",").map((n) => parseInt(n));
+const cards: Board[] = input.slice(1).map((b) =>
+  b.split("\n").map((l) =>
+    l
+      .split(" ")
+      .map((n) => parseInt(n))
+      .filter((n) => !isNaN(n))
+  )
+);
+
+const results: number[] = [];
+for (const d of drawn) {
+  for (let i = 0; i < cards.length; i++) {
+    cards[i] = filterBoard(cards[i], d);
+    if (cardComplete(cards[i])) {
+      results.push(scoreBoard(cards[i], d));
+      cards.splice(i, 1);
+      i -= 1;
+    }
+  }
+}
+
+export const solution = () => {
+  console.log("Day 4 Part 1:", results[0]);
+  console.log("Day 4 Part 2:", results[results.length - 1]);
+};
